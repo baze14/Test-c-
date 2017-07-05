50d4cabf626c433eee8a186bfa3ad0fab8235e94
baze14 <dsm_dase@yahoo.com>
2017-07-06 09:00:59 +1300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+
+typedef struct man {
+	int age;
+	 float size;
+	 char* name;
+	 man* friends [150];
+
+};
+
+int main(){
+
+    return 0;
+}
+
